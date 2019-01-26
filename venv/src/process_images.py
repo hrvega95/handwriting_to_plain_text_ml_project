b8a73d9ea23f47e1f419c66c11c5e6a25f9cd106
hector <hrvega95@gmail.com>
2019-01-26 15:10:48 -0800

    Added upscaling and downscaling for processing the images in the data set

--- venv/src/process_images.py
+++ venv/src/process_images.py
@@ -11,10 +11,19 @@ import time
 
 
 def convert_images_to_bw(image):
+    """
+    Converts an image to black and white 0 or 255 RGB values
+    :param image: cv2 image object that is to be converted to black and white
+    :return: returns the processed black and white image
+    """
     (thresh, im_bw) = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     return im_bw
 
 def make_all_images_bw():
+    """
+    this does through all the images in the data set and makes them all white and black. This is to help with processing
+    the data for feeding into the learner.
+    """
     root = os.path.dirname(__file__) + "/../Data/"
     for name_w_path in get_all_data_png(root):
         im_gray = cv2.imread(name_w_path, cv2.IMREAD_GRAYSCALE)
@@ -22,6 +31,10 @@ def make_all_images_bw():
         cv2.imwrite(name_w_path, im_bw)
 
 def crop_data_all_images(data_type):
+    """
+    Crops all of the images so that there is no extra space surrounding the black pixels.
+    :param data_type: word, sentence, or line. These are the three datat ypes in the data set.
+    """
     root = os.path.dirname(__file__) + "/../Data/" + str(data_type)
     for file_w_path in get_all_data_png(root):
         img = cv2.imread(file_w_path,cv2.IMREAD_GRAYSCALE)
@@ -29,6 +42,11 @@ def crop_data_all_images(data_type):
         cv2.imwrite(file_w_path, rect)
 
 def crop_single_image(image):
+    """
+    Crops a single image of all the extra white space around the blak pixels in the image
+    :param image: cv2 image object that is to be cropped
+    :return: returns the cropped image object after it has been processed
+    """
     img_flipped = 255 * (image < 128).astype(np.uint8)
     coords = cv2.findNonZero(img_flipped)
     x, y, w, h = cv2.boundingRect(coords)
@@ -36,10 +54,20 @@ def crop_single_image(image):
     return rect
 
 def find_contour_left_to_right(contour):
+    """
+    Helper function to sort the contours of a sentence or line data type so that they can be split into words in
+    seperate_lines_into_words
+    :param contour: contour object
+    :return: returns the right most point of the contour so we can sort left to right
+    """
     bound = cv2.boundingRect(contour)
     return bound[0]
 
 def seperate_lines_into_words():
+    """
+    Sepeartes all the line date type images in the data set into words and then stores them in directories that match
+    the names of the original image. Inside the new directory, all of the words will be names 1.png.... n.png.
+    """
     root = os.path.dirname(__file__) + "/../Data/lines/"
     for file_w_path in get_all_data_png(root):
         new_image_path = file_w_path.replace(".png", "")
@@ -65,6 +93,11 @@ def seperate_lines_into_words():
             cv2.imwrite(new_image_path + "/" + str(img_index) +  ".png", rect)
 
 def find_largest_image(data_type):
+    """
+    Finds the largest image in the data set. Has to have the largest width and height
+    :param data_type: can be line,sentence, or word
+    :return: the height and width of the largest image
+    """
     root = os.path.dirname(__file__) + "/../Data/" + data_type + "/"
     largest = np.zeros((1,1,1),np.uint8)
     for file_w_path in get_all_data_png(root):
@@ -75,6 +108,11 @@ def find_largest_image(data_type):
     return largest.shape[0], largest.shape[1]
 
 def find_smallest_image(data_type):
+    """
+    Finds the smallest image in the data set. Has to have the smallest width and height
+    :param data_type: can be line,sentence, or word
+    :return: the height and width of the smallest image
+    """
     root = os.path.dirname(__file__) + "/../Data/" + data_type + "/"
     smallest = np.zeros((10000, 10000, 1), np.uint8)
     for file_w_path in get_all_data_png(root):
@@ -84,8 +122,12 @@ def find_smallest_image(data_type):
     print("This is the smallest image by height " + str(smallest.shape[0]) + " and width " + str(smallest.shape[1]))
     return smallest.shape[0], smallest.shape[1]
 
-
 def get_all_data_png(root):
+    """
+    Grabs the path of all the png images in the data set under a specified root
+    :param root: The roor directory in which to start the search for all the images in the data set.
+    :return:
+    """
     list_png_paths = []
     for path, subdir, files in os.walk(root):
         for name in files:
@@ -94,6 +136,33 @@ def get_all_data_png(root):
                 list_png_paths.append(name_w_path)
     return list_png_paths
 
+def resize_images(data_type,scale="d"):
+    """
+    Resize all of the images of a specific data_type so that they will be the same size of either the smallest or the largest image in the data set.
+    :param data_type: Can be line, sentence, or word
+    :param scale: "d" is for downscale and "u" is for upscale
+    :return:
+    """
+    if scale == "d":
+        #downscale all the images so that they are the same size as the smallest image in the data set
+        #trying to test is downscaling, upcaling, or no scaling results in best ml performance
+        height, width = find_smallest_image(data_type)
+    elif scale == "u":
+        #option upscales all the images in the data set to match those of the largest image in the data set
+        height, width = find_largest_image(data_type)
+    root = os.path.dirname(__file__) + "/../Data/" + data_type + "/"
+    for file_w_path in get_all_data_png(root):
+        png_file_name = file_w_path.split("/")[-1]
+        #make sure to only downsize the images that were sepearted from lines/sentences to words
+        if len(png_file_name) < 10:
+            img = cv2.imread(file_w_path)
+            resized_img = cv2.resize(img,(width,height), interpolation=cv2.INTER_NEAREST)
+            png_directory = file_w_path.replace(png_file_name,"")
+            file_name = png_file_name.replace(".png","")
+            cv2.imwrite(png_directory+file_name+"_resized_"+scale+".png", resized_img)
+            
+        else:
+            continue
 
 if __name__ == "__main__":
-    find_largest_image("lines")
+    resize_images("lines", "u")
